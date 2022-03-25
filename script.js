c1dc953714a2278b462e3be514953e03432b2ac5
elipol02 <elipol02@icloud.com>
2022-03-25 15:53:55 -0700

    added single picture mode

--- script.js
+++ script.js
@@ -4,32 +4,79 @@ const oregon = [2,8];
 const northcascades = [3,10];
 const seattle = [4,7];
 
+//toggles
+var menuVisibility = false;
+var singlePictureMode = false;
+
 // Adds images to html
 function loadImages(portfolio){
     //clears images
     document.getElementById("images").innerHTML = ""; 
     document.getElementById("images2").innerHTML = "";
-
+    //clears menu
+    console.log(menuVisibility);
+    if (menuVisibility == true){
+        menu();
+        console.log("1");
+    }
     //gets portfolio information
     const imageCount = portfolio[1];
     const folder = portfolio[0];
-
     //creates array of image names and appends to collumns
     var images = [];
+    var a = [];
+    var divs = [];
     var i;
     for (i=1; i<= imageCount; i++){
         images[i] = document.createElement("img"); 
         images[i].src = "image-folders/" + folder + "/" + i + ".jpg";
+        a[i] = document.createElement("a");
+        a[i].href = "#";
+        a[i].setAttribute("class", "button");
+        a[i].setAttribute("onClick", "singlePicture(" + i + ", " + imageCount + ")");
+        a[i].appendChild(images[i]);
+        divs[i] = document.createElement("div");
+        divs[i].id = i;
+        divs[i].appendChild(a[i]);
         if (i <= imageCount/2){
-            document.getElementById("images").appendChild(images[i]);
+            document.getElementById("images").appendChild(divs[i]);
         }else{
-            document.getElementById("images2").appendChild(images[i]);
+            document.getElementById("images2").appendChild(divs[i]);
         }
+    }
+    //resets picture mode
+    if (singlePictureMode = true){
+        singlePicture(i, imageCount);
+    }
+}
+
+//toggles single picture mode
+function singlePicture(i, imageCount){
+    if (singlePictureMode == false){
+        for (let j=1; j<= imageCount; j++){
+            document.getElementById(j).style.display = "none";
+        }
+        document.getElementById(i).style.display = "inline";
+        const nodeList = document.querySelectorAll(".collumn");
+        for (let j = 0; j < nodeList.length; j++){
+            nodeList[j].style.flex = "100%";
+            nodeList[j].style.maxWidth = "100%";
+        }
+        singlePictureMode = true;
+    }else{
+        for (let j=1; j<= imageCount; j++){
+            document.getElementById(j).style.display = "inline";
+        }
+        const nodeList = document.querySelectorAll(".collumn");
+        for (let j = 0; j < nodeList.length; j++){
+            nodeList[j].style.flex = "50%";
+            nodeList[j].style.maxWidth = "50%";
+        }
+        singlePictureMode = false;
     }
 }
 
 // Toggles menu bar and its smooth animation
-var menuVisibility = false;
 function menu(){
     if (menuVisibility == false){
         document.getElementById("menu").style.display = "block";
